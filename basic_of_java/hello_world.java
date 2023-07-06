f0f93d6365f1a7b5f3c6f3502634e287e8eb7270
Pragatigt321 <gautampragati601@gmail.com>
2023-07-06 12:17:42 +0530

    my name

--- basic_of_java/hello_world.java
+++ basic_of_java/hello_world.java
@@ -2,6 +2,4 @@ package basic_of_java;
 
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
+        System.out.println("my name is pragati");
